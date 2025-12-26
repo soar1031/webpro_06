2c4d7d1249aa311341ef02287b2dc17f69d9fce0
soa1031 <s25g1024zv@s.chibakoudai.jp>
2025-12-26 22:24:09 +0900

    詳細

--- report/app_movie.js
+++ report/app_movie.js
@@ -6,6 +6,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
+// データ配列
 let movieList = [
     { id: 1, title: "トップガン マーヴェリック", year: 2022, rating: 5, review: "最高傑作！" },
     { id: 2, title: "ショーシャンクの空に", year: 1994, rating: 5, review: "希望を捨てないこと" },
@@ -20,7 +21,6 @@ app.get("/movies/create", (req, res) => {
     res.render('movie_create');
 });
 
-
 app.post("/movies", (req, res) => {
     const newId = movieList.length + 1;
     const newMovie = {
@@ -32,6 +32,18 @@ app.post("/movies", (req, res) => {
     };
     movieList.push(newMovie);
     res.redirect('/movies');
+});
+
+app.get("/movies/:id", (req, res) => {
+    const index = req.params.id;
+    if (isNaN(index)) return res.next();
+
+    const item = movieList[index];
+    if (item) {
+        res.render('movie_detail', { data: item, id: index });
+    } else {
+        res.send("データが見つかりません");
+    }
 });
 
 app.listen(8080, () => console.log("Movie App listening on port 8080!"));
