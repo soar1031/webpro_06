177b79744b5e432a4e6d568efc38a6ebac236053
soa1031 <s25g1024zv@s.chibakoudai.jp>
2025-12-26 23:42:33 +0900

    edit

--- report/app_muscle.js
+++ report/app_muscle.js
@@ -6,24 +6,21 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
-// データ配列（変数名: workoutList）
+
 let workoutList = [
     { id: 1, menu: "ベンチプレス", weight: 60, reps: 10, date: "2025-01-20" },
     { id: 2, menu: "スクワット", weight: 80, reps: 8, date: "2025-01-22" },
     { id: 3, menu: "チンニング", weight: 0, reps: 15, date: "2025-01-23" }
 ];
 
-// 1. 一覧表示
 app.get("/training", (req, res) => {
     res.render('training_list', { data: workoutList });
 });
 
-// 2. 新規登録画面
 app.get("/training/create", (req, res) => {
     res.render('training_create');
 });
 
-// 3. 新規登録処理
 app.post("/training", (req, res) => {
     const newId = workoutList.length + 1;
     const newWorkout = {
@@ -37,10 +34,8 @@ app.post("/training", (req, res) => {
     res.redirect('/training');
 });
 
-// 4. 詳細表示（ここを追加！）
 app.get("/training/:id", (req, res) => {
     const index = req.params.id;
-    // 数値かどうかチェック
     if (isNaN(index)) return res.next();
 
     const item = workoutList[index];
@@ -49,6 +44,27 @@ app.get("/training/:id", (req, res) => {
     } else {
         res.send("データが見つかりません");
     }
+});
+
+app.get("/training/edit/:id", (req, res) => {
+    const index = req.params.id;
+    const item = workoutList[index];
+    if (item) {
+        res.render('training_edit', { data: item, id: index });
+    } else {
+        res.redirect('/training');
+    }
+});
+
+app.post("/training/update/:id", (req, res) => {
+    const index = req.params.id;
+    if (workoutList[index]) {
+        workoutList[index].menu = req.body.menu;
+        workoutList[index].weight = req.body.weight;
+        workoutList[index].reps = req.body.reps;
+        workoutList[index].date = req.body.date;
+    }
+    res.redirect('/training');
 });
 
 app.listen(8080, () => console.log("Muscle App listening on port 8080!"));
